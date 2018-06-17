1894b4bef929b19d5a5271f2445d2cc1c1bfb48b
ivelasq <ivelasq@gmail.com>
2018-06-17 14:19:36 -0700

    initial r code

---
+++ ikea.R
@@ -0,0 +1,57 @@
+# Exploring IKEA Assembly Instructions
+
+# libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(rvest)
+library(pdftools)
+library(tesseract)
+
+# stackoverflow tutorial: https://stackoverflow.com/questions/31517121/using-r-to-scrape-the-link-address-of-a-downloadable-file-from-a-web-page
+
+# download ikea pdfs ----------------------------------------------------------
+
+# web scraping
+
+page <- read_html('https://www.ikea.com/ms/en_US/customer_service/assembly_instructions.html#7')
+
+pdf_fromsite <-
+  page %>%
+  html_nodes("a") %>%       # find all links
+  html_attr("href") %>%     # get the url
+  str_subset(".pdf") %>%   # find those that end in pdf
+  str_extract("(?<=open\\(\\').+?(?>pdf)") %>%
+  as_data_frame()
+
+# downloading pdfs
+
+pdf_urls <-
+  pdf_fromsite %>% 
+  mutate(urls = paste0("https://www.ikea.com/", value)) %>% 
+  select(-1) %>% 
+  slice(-1:-2) %>%
+  slice(1:5) %>% 
+  as_vector() 
+
+save_here <- paste0("./ikea_pdfs/assembly_instructions_", 1:5, ".pdf")
+
+mapply(download.file, pdf_urls, save_here)
+
+# reading pdf files -------------------------------------------------------
+
+pdfs_names <-
+  list.files(path = "./ikea_pdfs/")
+
+downloaded_pdfs <-
+  paste0("./ikea_pdfs/", pdfs_names)
+
+# converting to images
+
+mapply(pdf_convert, format = "png", pages = 2, dpi = 400, downloaded_pdfs)
+
+# reading image files
+
+png_names <-
+  list.files(path = "./", pattern = "\\.png$")
+
+mapply(ocr, png_names)
